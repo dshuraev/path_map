1d285944242cb56559df3c5c3c3ed14ace604e39
Daniil Shuraev <shuraev.daniil@gmail.com>
2025-12-05 12:52:19 -0500

    initial commit

---
+++ lib/path_map.ex
@@ -0,0 +1,18 @@
+defmodule PathMap do
+  @moduledoc """
+  Documentation for `PathMap`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PathMap.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/path_map_test.exs
@@ -0,0 +1,8 @@
+defmodule PathMapTest do
+  use ExUnit.Case
+  doctest PathMap
+
+  test "greets the world" do
+    assert PathMap.hello() == :world
+  end
+end
